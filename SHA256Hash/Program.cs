61eb416b4ee1b38a88eb41a1c921858f5e28f2b8
Mano Cardozo <manasses.cardozo@gmail.com>
2020-12-07 10:53:39 +1300

    Add project files.

---
+++ SHA256Hash/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace SHA256Hash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plainData = "plain data";
+            Console.WriteLine("Raw data: {0}", plainData);
+            string hashedData = ComputeSha256Hash(plainData);
+            Console.WriteLine("Hash {0}", hashedData);
+            Console.ReadLine();
+        }
+
+        static string ComputeSha256Hash(string rawData)
+        {
+            // Create a SHA256   
+            using var sha256Hash = SHA256.Create();
+
+            // ComputeHash - returns byte array  
+            byte[] bytes = sha256Hash.ComputeHash(Encoding.ASCII.GetBytes(rawData));
+
+            // Convert byte array to a string   
+            var builder = new StringBuilder();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                builder.Append(bytes[i].ToString("x2"));
+            }
+
+            return builder.ToString();
+        }
+    }
+}
